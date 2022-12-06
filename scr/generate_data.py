e224bd7634bdbb9b7c5b3707ff771ab79dc045a4
= <=>
2022-12-07 00:23:57 +0900

    - logistic data + gamma, unif data

--- scr/generate_data.py
+++ scr/generate_data.py
@@ -6,6 +6,29 @@ from lingam.utils import simulate_linear_sem
 from nptyping import NDArray
 
 
+def generate_unif(
+    mat: NDArray, seed: int = 123, n_sample: int = 1000, columns: list[str] | None = None
+) -> pd.DataFrame:
+    """generate samples data from given adjacency matrix"""
+    n_variable_full: Final[int] = mat.shape[0]
+    np.random.seed(seed)
+    x = np.zeros((n_variable_full, n_sample))
+    # x[6] = np.random.normal(size=n_sample)
+    x[6] = np.random.uniform(size=n_sample)
+    x[3] = np.dot(mat[3, :], x) + np.random.uniform(size=n_sample)
+    x[0] = np.dot(mat[0, :], x) + np.random.uniform(size=n_sample)
+    x[2] = np.dot(mat[2, :], x) + np.random.uniform(size=n_sample)
+    x[1] = np.dot(mat[1, :], x) + np.random.uniform(size=n_sample)
+    x[5] = np.dot(mat[5, :], x) + np.random.uniform(size=n_sample)
+    x[4] = np.dot(mat[4, :], x) + np.random.uniform(size=n_sample)
+    col_names: list[str] = (
+        columns
+        if columns is not None and len(columns) == n_variable_full
+        else [f"x{i}" for i in range(n_variable_full)]
+    )
+    return pd.DataFrame(np.array(x).T, columns=col_names)
+
+
 def generate_normal_unif(
     mat: NDArray, seed: int = 123, n_sample: int = 1000, columns: list[str] | None = None
 ) -> pd.DataFrame:
@@ -13,13 +36,35 @@ def generate_normal_unif(
     n_variable_full: Final[int] = mat.shape[0]
     np.random.seed(seed)
     x = np.zeros((n_variable_full, n_sample))
+    x[6] = np.random.normal(scale=0.25, size=n_sample)
     x[3] = np.dot(mat[3, :], x) + np.random.uniform(size=n_sample)
     x[0] = np.dot(mat[0, :], x) + np.random.uniform(size=n_sample)
     x[2] = np.dot(mat[2, :], x) + np.random.uniform(size=n_sample)
-    x[6] = np.random.normal(size=n_sample)
     x[1] = np.dot(mat[1, :], x) + np.random.uniform(size=n_sample)
     x[5] = np.dot(mat[5, :], x) + np.random.uniform(size=n_sample)
     x[4] = np.dot(mat[4, :], x) + np.random.uniform(size=n_sample)
+    col_names: list[str] = (
+        columns
+        if columns is not None and len(columns) == n_variable_full
+        else [f"x{i}" for i in range(n_variable_full)]
+    )
+    return pd.DataFrame(np.array(x).T, columns=col_names)
+
+
+def generate_gamma_unif(
+    mat: NDArray, seed: int = 123, n_sample: int = 1000, columns: list[str] | None = None
+) -> pd.DataFrame:
+    """generate samples data from given adjacency matrix"""
+    n_variable_full: Final[int] = mat.shape[0]
+    np.random.seed(seed)
+    x = np.zeros((n_variable_full, n_sample))
+    x[6] = np.random.gamma(shape=2.0, scale=0.5, size=n_sample)
+    x[3] = np.dot(mat[3, :], x) + np.random.uniform(low=-1.0, high=1.0, size=n_sample)
+    x[0] = np.dot(mat[0, :], x) + np.random.uniform(low=-1.0, high=1.0, size=n_sample)
+    x[2] = np.dot(mat[2, :], x) + np.random.uniform(low=-1.0, high=1.0, size=n_sample)
+    x[1] = np.dot(mat[1, :], x) - np.random.gamma(shape=2.0, scale=1.0, size=n_sample)
+    x[5] = np.dot(mat[5, :], x) - np.random.gamma(shape=2.0, scale=1.0, size=n_sample)
+    x[4] = np.dot(mat[4, :], x) - np.random.gamma(shape=2.0, scale=1.0, size=n_sample)
     col_names: list[str] = (
         columns
         if columns is not None and len(columns) == n_variable_full
--- scr/run_models.py
+++ scr/run_models.py
@@ -3,7 +3,7 @@ from typing import Final
 
 import pandas as pd
 from fns import get_amat, make_dot, run_model, save_dot
-from generate_data import generate_data_from_sem, generate_normal_unif
+from generate_data import generate_data_from_sem, generate_gamma_unif, generate_normal_unif, generate_unif
 from lingam import RCD, BottomUpParceLiNGAM
 from lingam.utils import make_prior_knowledge
 from nptyping import NDArray
@@ -18,7 +18,7 @@ def bup_with_sem_data(n_boot: int):
     save_dot(dot_true, name="bup_true")
 
     # generate sample data from the adjacency matrix given above
-    sem_types: Final[list[str]] = ["gauss", "exp", "gumbel", "logistic"]
+    sem_types: Final[list[str]] = ["gauss", "exp", "gumbel"]
     for sem_type in sem_types:
         print(f"{sem_type} model")
         X_full: pd.DataFrame = generate_data_from_sem(effect_true, sem_type=sem_type)
@@ -55,6 +55,27 @@ def bup_with_normal_unif_data(n_boot: int):
     run_model(model=model, data=X, n_boot=n_boot, name_graph="bup_est_normal_unif")
 
 
+def bup_with_unif_data(n_boot: int):
+    # true effect to estimate
+    effect_true: NDArray = get_amat()
+    # dot_true = make_dot(effect_true)
+    # save_dot(dot_true, name="bup_true")
+
+    # generate sample data from the adjacency matrix given above
+    X_full: pd.DataFrame = generate_unif(effect_true)
+    # let x6 unobservable
+    X = X_full.drop("x6", axis="columns")
+    n_variable: Final[int] = X.shape[1]
+
+    # define lingam model with prior
+    prior = make_prior_knowledge(
+        n_variables=n_variable,
+        sink_variables=[1, 4, 5],
+    )
+    model = BottomUpParceLiNGAM(prior_knowledge=prior)
+    run_model(model=model, data=X, n_boot=n_boot, name_graph="bup_est_unif")
+
+
 def rcd_with_sem_data(n_boot: int):
     # true effect to estimate
     effect_true: NDArray = get_amat()
@@ -62,7 +83,7 @@ def rcd_with_sem_data(n_boot: int):
     # save_dot(dot_true, name="bup_true")
 
     # generate sample data from the adjacency matrix given above
-    sem_types: Final[list[str]] = ["gauss", "exp", "gumbel", "logistic"]
+    sem_types: Final[list[str]] = ["gauss", "exp", "gumbel"]
     for sem_type in sem_types:
         print(f"{sem_type} model")
         X_full: pd.DataFrame = generate_data_from_sem(effect_true, sem_type=sem_type)
@@ -88,9 +109,42 @@ def rcd_with_normal_unif_data(n_boot: int):
     run_model(model=model, data=X, n_boot=n_boot, name_graph="rcd_est_normal_unif")
 
 
+def rcd_with_gamma_unif_data(n_boot: int):
+    # true effect to estimate
+    effect_true: NDArray = get_amat()
+    # dot_true = make_dot(effect_true)
+    # save_dot(dot_true, name="bup_true")
+
+    # generate sample data from the adjacency matrix given above
+    X_full: pd.DataFrame = generate_gamma_unif(effect_true)
+    # let x6 unobservable
+    X = X_full.drop("x6", axis="columns")
+
+    model = RCD()
+    run_model(model=model, data=X, n_boot=n_boot, name_graph="rcd_est_gamma_unif")
+
+
+def rcd_with_unif_data(n_boot: int):
+    # true effect to estimate
+    effect_true: NDArray = get_amat()
+    # dot_true = make_dot(effect_true)
+    # save_dot(dot_true, name="bup_true")
+
+    # generate sample data from the adjacency matrix given above
+    X_full: pd.DataFrame = generate_unif(effect_true)
+    # let x6 unobservable
+    X = X_full.drop("x6", axis="columns")
+
+    model = RCD()
+    run_model(model=model, data=X, n_boot=n_boot, name_graph="rcd_est_unif")
+
+
 if __name__ == "__main__":
     n_boot: Final[int] = 100
-    bup_with_sem_data(n_boot)
-    bup_with_normal_unif_data(n_boot)
-    rcd_with_sem_data(n_boot)
+    # bup_with_sem_data(n_boot)
+    # bup_with_normal_unif_data(n_boot)
+    # bup_with_unif_data(n_boot)
+    # rcd_with_sem_data(n_boot)
+    # rcd_with_unif_data(n_boot)
+    # rcd_with_gamma_unif_data(n_boot)
     rcd_with_normal_unif_data(n_boot)
